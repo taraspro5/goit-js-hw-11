9c90ebad68f94d3983d4bb37de0b8ed7bc487757
taraspro5 <kovtun_taras007@ukr.net>
2023-08-31 19:42:29 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,108 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const params = new URLSearchParams({
-  key: '39156572-72d7647317d1c76660d8c9d12',
+const form = document.querySelector('.search-form');
+const container = document.querySelector('.gallery');
+const guard = document.querySelector('.js-guard');
+const { searchQuery } = form;
+let page = 1;
+let totalToEnd = 0;
+
+const options = {
+  rootMargin: '300px',
+};
+const observer = new IntersectionObserver(handlerLoad, options);
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
 });
 
-axios.get(`https://pixabay.com/api/?${params}`).then(res => console.log(res));
+form.addEventListener('submit', handlerSubmit);
+
+async function handlerSubmit(evt) {
+  try {
+    evt.preventDefault();
+    page = 1;
+    const object = await serviceImage(page);
+    console.log(object);
+    if (!object.data.hits.length) {
+      throw new Error();
+    }
+    totalToEnd = object.data.totalHits;
+    Notiflix.Notify.success(`"Hooray! We found ${totalToEnd} images."`);
+    container.innerHTML = '';
+    container.insertAdjacentHTML('beforeend', createMarkup(object.data.hits));
+    observer.observe(guard);
+  } catch (err) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+}
+
+async function serviceImage(currentPage = '1') {
+  const params = new URLSearchParams({
+    key: '39156572-72d7647317d1c76660d8c9d12',
+    q: searchQuery.value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: currentPage,
+    per_page: 40,
+  });
+
+  return await axios.get(`https://pixabay.com/api/?${params}`);
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+      <a class="photo-link" href=${largeImageURL}>
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads ${downloads}</b>
+        </p>
+      </div>
+    </div>`
+    )
+    .join('');
+}
+
+async function handlerLoad(entries) {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting) {
+      page++;
+      totalToEnd -= 40;
+      if (totalToEnd <= 0) {
+        observer.unobserve(guard);
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      const object = await serviceImage(page);
+      container.insertAdjacentHTML('beforeend', createMarkup(object.data.hits));
+    }
+  });
+}
